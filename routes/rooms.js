5457e121e423ffa229d1dc200c1fc5aa57ef76cf
AnkitPatanaik <apatanai@purdue.edu>
2017-05-18 15:37:10 -0700

    added 2 more routes, need2test and debug

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,6 +5,32 @@ const util = require('util');
 
 var Room = require('../models/room');
 
+// get one room
+router.get('/getRoom', function (req, res) {
+      console.log('get room');
+      
+      Room.findOne({ name: req.params.id }, function (err, data) {
+              if (err) console.log(err);
+              else {
+                     console.log('found room'); 
+                     //do something to display room found in data
+              }
+      });
+});
+
+//get board for a room
+router.get('/getBoard', function (req, res) {
+      console.log('get board');
+      
+      Room.findOne({ name: req.params.id }, function (err, data) {
+              if (err) console.log(err);
+              else {
+                     console.log('found board'); 
+                     //do something to display it
+                     console.log(data.board);
+              }
+      });
+});
 //use when trying to make new room
 router.post('/createRoom', function (req, res) {
         console.log('update board in db');
@@ -35,7 +61,7 @@ router.post('/updateRoom', function (req, res) {
         console.log('update board in db');
         //console.log(util.inspect(req.body.board, { showHidden: true, depth: null }));
         // search for room w/name, update corresponding board
-        Room.findOne({ name: req.body.name }, function (err, date) {
+        Room.findOne({ name: req.body.name }, function (err, data) {
               if (err) console.log(err);
               else {
                      data.board = req.body.board;
