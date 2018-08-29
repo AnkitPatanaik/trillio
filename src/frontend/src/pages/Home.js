c54c070a48cf24de686f4aa37dd5883185366bb4
AnkitPatanaik <apatanai@purdue.edu>
2018-08-28 23:13:55 -0700

    add fetch client, add state to register page

--- src/frontend/src/pages/Home.js
+++ src/frontend/src/pages/Home.js
@@ -2,13 +2,35 @@ import React from "react";
 import InputComponent from "../components/InputComponent";
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentials: {
+        email: '',
+        password: '',
+      },
+    };
+  }
+
+  onChange = (event) => {
+    const nextCredentials = this.state.credentials;
+    nextCredentials[event.target.name] = event.target.value;
+    return this.setState({ credentials: nextCredentials });
+  };
+
+  onSave = (event) => {
+    event.preventDefault();
+  }
+
   render = () => {
     return (
       <div>
         <center>
-          <b>Trillio</b>
-          <InputComponent title="Email" name="email" type="email" />
-          <InputComponent title="Password" name="password" type="password" />
+          <b>Trillio </b>
+          <InputComponent title="Username" name="username" onChange={this.onChange} type="username" />
+          <InputComponent title="Password" name="password" onChange={this.onChange} type="password" />
+          <button onClick={this.onSave}>Register</button>
         </center>
       </div>
     );
